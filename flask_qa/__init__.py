7af83b18909eb07ff82fec7967dc96d157a7cfc9
IzziCooki <63527400+IzziCooki@users.noreply.github.com>
2021-07-05 15:37:22 -0400

    Update __init__.py

--- flask_qa/__init__.py
+++ flask_qa/__init__.py
@@ -10,7 +10,7 @@ def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
-    
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://dbsvretlwljtwh:45879767620cde7aa549b5fdf0985a22f8bf59ae94b861a6d914f39ecd4d034a@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d98q49rbnjpd4"
 
     db.init_app(app)
 
