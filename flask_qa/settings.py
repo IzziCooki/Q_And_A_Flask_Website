1831e7be134f691bc83be97d6257dabd23f6c248
IzziCooki <63527400+IzziCooki@users.noreply.github.com>
2021-07-05 14:29:04 -0400

    Update settings.py

--- flask_qa/settings.py
+++ flask_qa/settings.py
@@ -1,4 +1,4 @@
 import os
-SQL_DATABASE_URI= os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SQLALCHEMY_TRACK_MODIFICATION_ = False
