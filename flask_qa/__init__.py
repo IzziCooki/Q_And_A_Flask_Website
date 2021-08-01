f92d0a5748ac8d1dd455cdd48818010c30a859ed
IzziCooki <63527400+IzziCooki@users.noreply.github.com>
2021-08-01 16:07:12 -0400

    Update __init__.py

--- flask_qa/__init__.py
+++ flask_qa/__init__.py
@@ -6,10 +6,10 @@ from .models import User
 from .routes.auth import auth
 from .routes.main import main
 
-def create_app(config_file='settings.py'):
+def create_app():
     app = Flask(__name__)
 
-    app.config.from_pyfile(config_file)
+    app.config["SECRET_KEY"]="dslhfjkshdlfjsvnksdjfnsf"
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://dbsvretlwljtwh:45879767620cde7aa549b5fdf0985a22f8bf59ae94b861a6d914f39ecd4d034a@ec2-54-163-254-204.compute-1.amazonaws.com:5432/d98q49rbnjpd4"
 
     db.init_app(app)
